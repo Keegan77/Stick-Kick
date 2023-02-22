4274ee3bf3474d9216a63befa599f2fca8a0b5c9
Keegan77 <keegansb77@gmail.com>
2023-02-22 16:55:29 -0500

    Added dynamic camera scaling

--- Assets/Scripts/CameraScaling.cs
+++ Assets/Scripts/CameraScaling.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class CameraScaling : MonoBehaviour
 {
-    public BoxCollider2D rink;
+    public BoxCollider2D boundCollider;
+    public float _buffer;
+    Camera cam;
+
+    private void Awake()
+    {
+        boundCollider = GameObject.FindGameObjectWithTag("Bounds").GetComponent<BoxCollider2D>();
+        cam = GetComponent<Camera>();
+    }
     void Start()
     {
-        float orthoSize = rink.size.x * Screen.height / Screen.width;
+        
+        var (center, size) = CalculateOrthoSize();
 
-        Camera.main.orthographicSize = orthoSize;
+        cam.transform.position = center;
+        cam.orthographicSize = size;
+       
+    }
+    private (Vector3 center, float size) CalculateOrthoSize()
+    {
+        var bounds = new Bounds();
+        bounds.Encapsulate(boundCollider.bounds);
+        bounds.Expand(_buffer);
+
+        var vertical = bounds.size.y;
+        var horizontal = bounds.size.x * cam.pixelHeight / cam.pixelWidth; // pixel height / pixel width = aspect ratio
+        var size = Mathf.Max(horizontal, vertical) * 0.5f;
+        var center = bounds.center + new Vector3(0, 0, -10);
+
+        return (center, size);
     }
 }
